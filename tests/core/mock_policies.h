f32ef82e6d2d98eb8ac855f318c2b0da50b2391d
Tomas Vaskevicius <vaskevicius.tomas@gmail.com>
2017-05-01 17:27:22 +0100

    Added unit tests for piecewise-deterministic Markov processes host class.

---
+++ tests/core/mock_policies.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <gmock/gmock.h>
+
+#include "core/pdmp.h"
+
+/**
+ * In this header file we define mock policies for piecewise-deterministic
+ * Markov process simulation.
+ */
+
+// Forward declare mock classes.
+struct MockIntensity;
+struct MockPoissonProcess;
+struct MockMarkovKernel;
+struct MockFlow;
+
+// Typedef the dummy Pdmp type to be used for unit testing.
+typedef pdmp::Pdmp<
+          MockPoissonProcess, MockMarkovKernel, MockIntensity, MockFlow>
+        DummyPdmp;
+
+struct DummyState {
+  using RealType = float;
+  DummyState(int state) : state(state) {}
+  int state;
+};
+
+bool operator==(const DummyState& lhs, const DummyState& rhs) {
+  return lhs.state == rhs.state;
+}
+
+struct MockPoissonProcess {
+  MockPoissonProcess() : MockPoissonProcess(0) {};
+  MockPoissonProcess(int dummyVariable)
+    : dummyPoissonProcessVariable(dummyVariable) {};
+  const int dummyPoissonProcessVariable;
+  MOCK_METHOD2(getJumpTime, float(const DummyState&, const DummyPdmp&));
+};
+
+struct MockMarkovKernel {
+  MockMarkovKernel() : MockMarkovKernel(0) {};
+  MockMarkovKernel(int dummyVariable)
+    : dummyMarkovKernelVariable(dummyVariable) {};
+  const int dummyMarkovKernelVariable;
+  MOCK_METHOD2(jump, DummyState(const DummyState&, const DummyPdmp&));
+};
+
+struct MockIntensity {
+  MockIntensity() : MockIntensity(0) {};
+  MockIntensity(int dummyVariable) : dummyIntensityVariable(dummyVariable) {};
+  int dummyIntensityVariable;
+  MOCK_METHOD1(evaluateIntensityAt, float(const DummyState&));
+};
+
+struct MockFlow {
+  MockFlow() : MockFlow(0) {};
+  MockFlow(int dummyVariable) : dummyFlowVariable(dummyVariable) {};
+  const int dummyFlowVariable;
+  MOCK_METHOD2(advanceStateByFlow, DummyState(const DummyState&, float));
+};
---
+++ tests/core/pdmp_tests.cc
@@ -0,0 +1,95 @@
+#include <tuple>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "core/pdmp.h"
+#include "mock_policies.h"
+
+/**
+ * We will here test the behavious of the host class for assembling
+ * piecewise-deterministic Markov processes.
+ */
+
+
+using namespace testing;
+
+TEST(PdmpHostClassTests, TestPdmpWithMockPolicies) {
+  DummyPdmp pdmp;
+  const float jumpTime = 10.0f;
+  const DummyState initialState(0);
+  const DummyState stateAfterFlow(1);
+  const DummyState stateAfterJump(2);
+
+  using ResultType = pdmp::IterationResult<DummyState>;
+  ResultType expectedResult(stateAfterJump, jumpTime);
+
+  // Set up the mock Poisson process policy.
+  EXPECT_CALL(
+    *static_cast<MockPoissonProcess*>(&pdmp),
+    getJumpTime(initialState, _))
+      .Times(1)
+      .WillOnce(Return(jumpTime));
+
+  // Set up the mock flow policy.
+  EXPECT_CALL(
+    *static_cast<MockFlow*>(&pdmp),
+    advanceStateByFlow(initialState, jumpTime))
+      .Times(1)
+      .WillOnce(Return(stateAfterFlow));
+
+  // Set up the mock Markov kernel policy.
+  EXPECT_CALL(
+    *static_cast<MockMarkovKernel*>(&pdmp), jump(stateAfterFlow, _))
+      .Times(1)
+      .WillOnce(Return(stateAfterJump));
+
+  auto actualResult = pdmp.simulateOneIteration(initialState);
+  EXPECT_TRUE(actualResult == expectedResult);
+}
+
+TEST(PdmpConstructorsTests, TestDefaultConstructor) {
+  DummyPdmp pdmp;
+
+  EXPECT_TRUE(pdmp.dummyPoissonProcessVariable == 0);
+  EXPECT_TRUE(pdmp.dummyMarkovKernelVariable == 0);
+  EXPECT_TRUE(pdmp.dummyIntensityVariable == 0);
+}
+
+TEST(PdmpConstructorsTests, TestConstructorUnwrappingTuples) {
+  DummyPdmp pdmp(
+    std::make_tuple(1),
+    std::make_tuple(2),
+    std::make_tuple(3));
+
+  EXPECT_TRUE(pdmp.dummyPoissonProcessVariable == 1);
+  EXPECT_TRUE(pdmp.dummyMarkovKernelVariable == 2);
+  EXPECT_TRUE(pdmp.dummyIntensityVariable == 3);
+}
+
+TEST(PdmpConstructorsTests, TestConstructorWithEmptyTuple) {
+  DummyPdmp pdmp(
+    std::make_tuple(1),
+    std::make_tuple(2),
+    std::make_tuple());
+
+  EXPECT_TRUE(pdmp.dummyPoissonProcessVariable == 1);
+  EXPECT_TRUE(pdmp.dummyMarkovKernelVariable == 2);
+  EXPECT_TRUE(pdmp.dummyIntensityVariable == 0);
+}
+
+TEST(PdmpConstructorsTests, TestDefaultArgumentForIntensityTuple) {
+  DummyPdmp pdmp(
+    std::make_tuple(1),
+    std::make_tuple(2));
+
+  EXPECT_TRUE(pdmp.dummyPoissonProcessVariable == 1);
+  EXPECT_TRUE(pdmp.dummyMarkovKernelVariable == 2);
+  EXPECT_TRUE(pdmp.dummyIntensityVariable == 0);
+
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
